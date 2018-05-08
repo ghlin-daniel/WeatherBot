886f9d765fb528e4a8cdcd0d068a77c2d29505e1
Daniel Lin <brickgit@gmail.com>
2018-05-07 20:14:58 -0700

    Update message format

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ import requests
 from flask import Flask, request, jsonify
 import googlemaps
 
-app = Flask(__name__)
-
+# Keys
 google_geocoding_api_key = os.environ.get('GOOGLE_GEOCODING_API_KEY')
-gmaps = googlemaps.Client(key=google_geocoding_api_key)
-
 dark_sky_api_key = os.environ.get('DARK_SKY_API_KEY')
 
-error = "I don't know the weather but I hope it's good!"
+app = Flask(__name__)
+gmaps = googlemaps.Client(key=google_geocoding_api_key)
+
+dark_sky_api_url = "https://api.darksky.net/forecast/%s/%f,%f,%s?" \
+                   "exclude=currently,minutely,hourly,alerts,flags&lang=zh-tw&units=si"
+
+weather_msg = "%s\n" \
+              "%s\n" \
+              "氣溫: %s℃ ~ %s℃\n" \
+              "體感溫度: %s℃ ~ %s℃"
+error_msg = "I don't know the weather but I hope it's good!"
 
 
 @app.route("/", methods=['POST'])
@@ -20,24 +27,24 @@ def fb_bot():
     query_result = json.loads(request.data).get('queryResult', {})
     intent = query_result.get('intent', {})
     if intent.get('displayName') != 'question_what_is_the_weather':
-        return jsonify(fulfillmentText=error)
+        return jsonify(fulfillmentText=error_msg)
 
     parameters = query_result.get('parameters', {})
     query_city = parameters.get('geo-city', '')
     query_date = parameters.get('date', '')
 
     if query_city == '' or query_date == '':
-        return jsonify(fulfillmentText=error)
+        return jsonify(fulfillmentText=error_msg)
 
     location = get_location(query_city)
     lat = location.get('lat', None)
     lng = location.get('lng', None)
     if lat is None or lng is None:
-        return jsonify(fulfillmentText=error)
+        return jsonify(fulfillmentText=error_msg)
 
     weather = get_weather(lat, lng, query_city, query_date)
     if weather is None:
-        return jsonify(fulfillmentText=error)
+        return jsonify(fulfillmentText=error_msg)
 
     return jsonify(fulfillmentText=weather)
 
@@ -51,9 +58,7 @@ def get_location(query_city):
 
 
 def get_weather(lat, lng, query_city, query_date):
-    url = 'https://api.darksky.net/forecast/%s/%f,%f,%s?' \
-          'exclude=currently,minutely,hourly,alerts,flags&lang=zh-tw&units=si' \
-          % (dark_sky_api_key, lat, lng, query_date)
+    url = dark_sky_api_url % (dark_sky_api_key, lat, lng, query_date)
 
     response = requests.get(url)
     json_response = response.json()
@@ -69,9 +74,6 @@ def get_weather(lat, lng, query_city, query_date):
     apparent_temperature_low = data.get('apparentTemperatureLow', '')
     apparent_temperature_high = data.get('apparentTemperatureHigh', '')
 
-    return '%s\n' \
-           '%s\n' \
-           '氣溫：%s ~ %s\n' \
-           '體感溫度：%s ~ %s' \
-           % (query_city, summary, temperature_low, temperature_high,
-              apparent_temperature_low, apparent_temperature_high)
+    return weather_msg % (query_city, summary,
+                          temperature_low, temperature_high,
+                          apparent_temperature_low, apparent_temperature_high)
